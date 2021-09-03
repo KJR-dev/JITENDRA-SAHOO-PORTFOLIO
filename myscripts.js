fbef509c2aae333e8889f8dceef1174fc74508f2
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2021-09-03 14:25:13 +0530

    Initial commit

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+function Name() {
+    alert("ENTER ONLY CHARACTER");
+};
+function phone() {
+    alert("ENTER ONLY 10 DIGITS NUMBER");
+};
+function email() {
+    alert("ENTER VALID EMAIL");
+};
+function submit() {
+    alert("SUBMIT SUCCESSFUL");
+};
